987b9482cea977124814f1deaac85b1527f1fc8c
leesam <kofsiwon@siwonschool.com>
2020-10-13 23:12:54 +0900

    Initial Commit

---
+++ LSJailBreakChecker/LSJailBreakChecker.h
@@ -0,0 +1,18 @@
+//
+//  LSJailBreakChecker.h
+//  LSJailBreakChecker
+//
+//  Created by 영준 이 on 2020/10/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LSJailBreakChecker.
+FOUNDATION_EXPORT double LSJailBreakCheckerVersionNumber;
+
+//! Project version string for LSJailBreakChecker.
+FOUNDATION_EXPORT const unsigned char LSJailBreakCheckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSJailBreakChecker/PublicHeader.h>
+
+
